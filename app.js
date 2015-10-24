a7c5a840351ccc27ce960137d710ab304b2e29f2
jeff <jefflanzi@gmail.com>
2015-10-24 16:56:09 -0700

    app.js change console.log to report listening on port 3000

--- app.js
+++ app.js
@@ -8,4 +8,4 @@ app.use('/applicants', applicants);
 
 // Start the application
 app.listen(3000);
-console.log('Application Server Listening on port 80');
+console.log('Application Server Listening on port 3000');
