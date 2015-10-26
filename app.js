10fd85ad7dc1f93421f4cc2ae5c9834a4fb82c38
Louis Lister <llister@repairturtle.com>
2015-10-25 23:41:34 -0700

    Updated app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var applicants = require('./routes/applicants.js');
 
-<<<<<<< HEAD
 app.set('view engine', 'jade');
 
 app.use('/', express.static(__dirname + '/public'));
@@ -22,9 +21,5 @@ app.use('/applicants', applicants);
 
 // Start the application
 app.listen(3000);
-<<<<<<< HEAD
-console.log('Listening on port 3000');
 console.log('Welcome to Learn To Code OC Server');
-=======
 console.log('Application Server Listening on port 3000');
->>>>>>> 3674b76501a6be59189c69a28d38dcea0f3e639f
