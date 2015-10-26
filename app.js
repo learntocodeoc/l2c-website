be7ba0c35439d39136d1359712e2efc9f6358ba1
Louis Lister <llister@repairturtle.com>
2015-10-25 23:10:34 -0700

    All updates with app.js

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'jade');
+
+app.use('/', express.static(__dirname + '/public'));
+
 
 // Default home route
 app.get('/', function(req, res){
-	res.send('default home directory');
+res.render('index');
 });
+
+
 
 // Start the application
 app.listen(3000);
 console.log('Listening on port 3000');
+console.log('Welcome to Learn To Code OC Server');
