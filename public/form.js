ceaa07989b90284fa419349a250c8e3b10300cae
jeff <jefflanzi@gmail.com>
2015-10-26 23:32:32 -0700

    form.js construction of json object

--- public/form.js
+++ public/form.js
@@ -1,29 +1,48 @@
 $(document).ready(function(){
 
-    $.fn.serializeObject = function()
-    {
-        var object = {};
-        var a = this.serializeArray();
-        // console.log(a);
-        $.each(a, function() {
-            object[this.name] = this.value || '';
-        });
-        return object;
-    };
+  $('#submitButton').on('click', function(){
 
-    $('#submitButton').on('click', function(){
-        var jsonObject = JSON.stringify($('#form1').serializeObject());
-        $('#result').text(jsonObject);
-        $.ajax({
-            type: "POST",
-            url: "/applicants",
-            data: jsonObject,
-            success: function(data){
-                console.log("data_saved!")
-            }, error: function(data){
-                console.log("error thrown");
-            }
-        });
-        return false;
-    })
+    // serialize form to array of objects
+    var a = $('#form1').serializeArray();
+    // create lookup reference object
+    var lookup = {};
+    for (var i = 0, len = a.length; i < len; i++) {
+        lookup[a[i].name] = a[i];
+    }
+
+    var formData = {};
+    formData.orgName = lookup.orgName.value;
+    formData.webUrl = lookup.webUrl.value;
+    var primaryContact = {};
+    primaryContact.firstName = lookup.contactName.value;
+    primaryContact.lastName = lookup.contactLastName.value;
+    primaryContact.email = lookup.contactEmail.value;
+    primaryContact.contactNumber = lookup.contactNumber.value;
+    formData.primaryContact = primaryContact
+    var webContact = {};
+    webContact.firstName = lookup.webContactName.value;
+    webContact.lastName = lookup.webContactLastName.value;
+    webContact.email = lookup.emailAddress.value;
+    webContact.phone = lookup.phoneNumber.value;
+    formData.webContact = webContact;
+
+
+    var jsonString = JSON.stringify(formData);
+
+    console.log(jsonString);
+    $('#result').text(jsonString);
+
+    $.ajax({
+        method: "POST",
+        contentType: "application/json",
+        url: "/applicants",
+        data: jsonString,
+        success: function(data){
+            console.log("data_saved!")
+        }, error: function(data){
+            console.log("error thrown");
+        }
+    });
+    return false;
+  })
 });
