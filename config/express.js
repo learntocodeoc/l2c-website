d791ab3523a995fa3baaa016e5b3a04c0ff315a5
jeff <jefflanzi@gmail.com>
2015-11-05 22:44:09 -0800

    refactored body-parser out of routes/applicants.js and into config/express.js

--- config/express.js
+++ config/express.js
@@ -3,12 +3,17 @@ var express = require('express');
 module.exports = function() {
   var app = express();
   var applicants = require('../routes/applicants.js');
+  var bodyParser = require('body-parser');
 
   // Jade Templates Config
   app.set('views', './views');
   app.set('view engine', 'jade');
 
-  // Routes
+  // Parse json from incoming request bodies
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({"extended" : false}));
+
+  // Mount routs to application
   app.use(express.static('./public'));
   app.use('/applicants', applicants);
 
